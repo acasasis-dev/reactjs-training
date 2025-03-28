a0f29b1d322e0d8ae68fcf6053ef3315c0a946ec
acasasis-dev <angelito.casasis@gmail.com>
2025-03-29 04:15:34 +0800

    Refactoring for more readable code

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,10 +5,12 @@ function getRandomInt(max: number) {
 }
 
 function Header() {
+	const prefix = prefixes[getRandomInt(2)];
+
 	return (
 		<>
 			<h1>This is a header for React Training</h1>
-			<p>{prefixes[getRandomInt(2)]} building a training react app for frontend learning.</p>
+			<p>{prefix} building a training react app for frontend learning.</p>
 		</>
 	);
 }
