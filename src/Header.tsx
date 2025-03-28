0815333eb7f2d5da0f5e48fcafd2f0678935a380
acasasis-dev <angelito.casasis@gmail.com>
2025-03-29 04:13:55 +0800

    Added dynamic values

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,14 @@
+const prefixes = ["Awesomely", "Boldly", "Savagely"];
+
+function getRandomInt(max: number) {
+	return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
 	return (
 		<>
 			<h1>This is a header for React Training</h1>
+			<p>{prefixes[getRandomInt(2)]} building a training react app for frontend learning.</p>
 		</>
 	);
 }
